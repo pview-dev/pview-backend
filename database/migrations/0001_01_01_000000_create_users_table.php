c283c2432f09d5715e824bbaf3c1efea80db529d
amirithm <amirithm01@gmail.com>
2025-10-30 21:09:20 +0330

    add migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('github_id')->nullable()->unique();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
+            $table->string('number')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('role')->default('guest');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_10_30_173252_create_contact_lists_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate();
+            $table->string('label');
+            $table->string('url');
+            $table->unsignedTinyInteger('order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_lists');
+    }
+};
